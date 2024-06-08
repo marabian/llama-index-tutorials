4299f5783ab2ed856fddb928d6de6ae2818f63de
Mher <mherarabian@gmail.com>
2024-06-07 19:00:49 -0700

    changes

--- utils.py
+++ utils.py
@@ -62,9 +62,9 @@ def get_doc_tools(
     vector_index = VectorStoreIndex(nodes)
 
     def vector_query(query: str, page_numbers: Optional[List[str]] = None) -> str:
-        """Use to answer questions over the MetaGPT paper.
+        """Use to answer questions over a given paper.
 
-        Useful if you have specific questions over the MetaGPT paper.
+        Useful if you have specific questions over the paper.
         Always leave page_numbers as None UNLESS there is a specific page you want to search for.
 
         Args:
@@ -94,10 +94,65 @@ def get_doc_tools(
     summary_tool = QueryEngineTool.from_defaults(
         name=f"summary_tool_{name}",
         query_engine=summary_query_engine,
-        description=(
-            "Use ONLY IF you want to get a holistic summary of MetaGPT. "
-            "Do NOT use if you have specific questions over MetaGPT."
-        ),
+        description=(f"Useful for summarization questions related to {name}"),
     )
 
     return vector_query_tool, summary_tool
+
+
+# def get_doc_tools(
+#     file_path: str,
+#     name: str,
+# ) -> str:
+#     """Get vector query and summary query tools from a document."""
+
+#     # load documents
+#     documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
+#     splitter = SentenceSplitter(chunk_size=1024)
+#     nodes = splitter.get_nodes_from_documents(documents)
+#     vector_index = VectorStoreIndex(nodes)
+
+#     def vector_query(
+#         query: str,
+#         filter_key_list: List[str],
+#         filter_value_list: List[str]
+#     ) -> str:
+#         """Perform a vector search over an index.
+
+#         query (str): the string query to be embedded.
+#         filter_key_list (List[str]): A list of metadata filter field names
+#             Must specify ['page_label'] or empty list. Please leave empty
+#             if there are no explicit filters to specify.
+#         filter_value_list (List[str]): List of metadata filter field values
+#             (corresponding to names specified in filter_key_list)
+
+#         """
+#         metadata_dicts = [
+#             {"key": k, "value": v} for k, v in zip(filter_key_list, filter_value_list)
+#         ]
+
+#         query_engine = vector_index.as_query_engine(
+#             similarity_top_k=2,
+#             filters=MetadataFilters.from_dicts(metadata_dicts)
+#         )
+#         response = query_engine.query(query)
+#         return response
+
+#     vector_query_tool = FunctionTool.from_defaults(
+#         fn=vector_query,
+#         name=f"vector_query_{name}"
+#     )
+
+#     summary_index = SummaryIndex(nodes)
+#     summary_query_engine = summary_index.as_query_engine(
+#         response_mode="tree_summarize",
+#         use_async=True,
+#     )
+#     summary_tool = QueryEngineTool.from_defaults(
+#         query_engine=summary_query_engine,
+#         name=f"summary_query_{name}",
+#         description=(
+#             f"Useful for summarization questions related to {name}"
+#         ),
+#     )
+#     return vector_query_tool, summary_tool
